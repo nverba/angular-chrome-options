12db20c99289adb72a5d9f67ea4206e63063979f
nverba <distantstate@gmail.com>
2014-08-31 17:20:46 +0100

    refactored tests

--- tests/chrome-options-service/chrome-options-service_test.js
+++ tests/chrome-options-service/chrome-options-service_test.js
@@ -1,15 +1,21 @@
 /*global console, beforeEach, afterEach, describe, inject, module, it */
 
-describe("optionsService", function () {
+describe("optionsService:", function () {
   "use strict";
 
   var options, $rootScope, digest;
 
   var storageAPI = {
-    set:    function () {},
-    get:    function () {},
-    remove: function () {},
-    clear:  function () {}
+    local : {
+      set:    function () {},
+      get:    function () {},
+      remove: function () {},
+      clear:  function () {}
+    },
+
+    onChanged: {
+      addListener: function () {}
+    }
   };
 
   var exampleConfig = [
@@ -27,96 +33,85 @@ describe("optionsService", function () {
     }
   ];
 
-  var expectedDefaults = { testcat_a: { option_a1: 4 }, testcat_b: { option_b1: 5, option_b2: 6, }, testcat_c: { option_c1: 4 }, testcat_d: { option_e1: 5 }};
-  var updatedDefaults = { testcat_a: { option_a1: 100 }, testcat_b: { option_b1: 5, option_b2: 6, }, testcat_c: { option_c1: 4 }, testcat_d: { option_e1: 5 }};
-
-  var spyStorageSet    = sinon.spy(storageAPI, "set");
-  var spyStorageGet    = sinon.stub(storageAPI, "get").callsArgWith(1, { 'clearCodeOptions': expectedDefaults });
-  var spyStorageRemove = sinon.spy(storageAPI, "remove");
-  var spyStorageClear  = sinon.spy(storageAPI, "clear");
-
-
-  beforeEach(module('optionsService'));
+  var expected_defaults = { testcat_a: { option_a1: 4 }, testcat_b: { option_b1: 5, option_b2: 6, }, testcat_c: { option_c1: 4 }, testcat_d: { option_e1: 5 }};
+  var updated_defaults  = { testcat_a: { option_a1: 100 }, testcat_b: { option_b1: 5, option_b2: 6, }, testcat_c: { option_c1: 4 }, testcat_d: { option_e1: 5 }};
+  var spyStorageSet     = sinon.spy(storageAPI.local, "set");
+  var spyStorageGet     = sinon.stub(storageAPI.local, "get").callsArgWith(1, { 'clearCodeOptions': angular.copy(expected_defaults) });
 
   beforeEach(function () {
+
+    module('optionsService');
+
     angular.mock.module("optionsConfig", function ($provide) {
       $provide.value('config', exampleConfig);
     });
-  });
 
-  beforeEach(function () {
     module(function($provide){
-      $provide.value('$window', { chrome: { storage: { local: storageAPI }}});
+      $provide.value('$window', { chrome: { storage: storageAPI }});
+    });
+
+    inject(function(_options_, _$rootScope_) {
+      options = _options_;
+      $rootScope = _$rootScope_;
+      $rootScope.options = options.categories;
+
+      digest = function () {
+        setTimeout(function () {
+          $rootScope.$digest();
+        }, 0);
+      };
     });
   });
 
-  beforeEach(inject(function(_options_, _$rootScope_) {
-    options = _options_;
-    $rootScope = _$rootScope_;
 
-    digest = function () {
-      setTimeout(function () {
-        $rootScope.$digest();
-      }, 0);
-    };
-  }));
-
-  describe("app init:", function () {
-
-    it(".ready should return a promise", function () {
-
-      digest();
-
-      return options.ready.should.be.fulfilled;
-
-    });
-
-    it("map defaults into chrome.local.storage", function () {
-
-      sinon.assert.calledWith(spyStorageSet, { 'clearCodeOptions': expectedDefaults });
-
-    });
-
-    it("exposes a category object", function (done) {
-
-      digest();
-
-      options.ready.then(function () {
-        assert.deepEqual(options.categories, expectedDefaults);
-        done();
+  describe("on initialise:", function () {
+    describe(".ready:", function () {
+      it("should return a promise", function () {
+        digest();
+        return options.ready.should.be.fulfilled;
       });
+    });
 
+    describe("chrome.local.storage.set:", function () {
+      it("was called with expected defaults", function () {
+        sinon.assert.calledWith(spyStorageSet, { 'clearCodeOptions': expected_defaults });
+      });
+    });
+
+    describe("options.categories:", function () {
+      it("returns the default categories object", function () {
+        digest();
+        return assert.deepEqual(options.categories, expected_defaults);
+      });
     });
   });
 
-  describe("functions:", function () {
-
-    it("maps config to categories of default values, substituting aliased option names", function () {
-
-      assert.deepEqual(options.mapDefaults(exampleConfig), expectedDefaults);
-
+  describe("options functions:", function () {
+    describe(".mapDefaults:", function () {
+      it("maps config data to default categories object, substituting aliased option names", function () {
+        return assert.deepEqual(options.mapDefaults(exampleConfig), expected_defaults);
+      });
     });
   });
 
-  describe("save/update:", function () {
-
-    it("push option updates to local storage", function (done) {
-
-      digest();
-
-      options.ready.then(function () {
-        $rootScope.options = options.categories;
-
+  describe("options scope:", function () {
+    afterEach(function () {
+      $rootScope.options.testcat_a.option_a1 = 4;
+    });
+    describe("update scope:", function () {
+      it("pushes updates to local storage", function () {
         $rootScope.options.testcat_a.option_a1 = 100;
+        sinon.assert.calledWith(spyStorageSet, { 'clearCodeOptions': updated_defaults });
+      }) ;
+    }) ;
+  });
 
-        sinon.assert.calledWith(spyStorageSet, { 'clearCodeOptions': updatedDefaults });
-
-        done();
+  describe("options storage", function () {
+    describe("update storage:", function () {
+      it("updates scope on local storage changes", function () {
+        //console.log(options.categories);
+        //return assert.deepEqual(options.categories, updated_defaults);
       });
-    });
-
-    it.skip("can update user style sheets on theme selection", function () {
-
     });
   });
 
