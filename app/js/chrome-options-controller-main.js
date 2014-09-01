2df032cf264f327b1c834d953de16b873fe51141
nverba <distantstate@gmail.com>
2014-09-01 11:50:43 +0100

    added selectPage function to scope

--- app/js/chrome-options-controller-main.js
+++ app/js/chrome-options-controller-main.js
@@ -3,6 +3,10 @@ ChromeOptionsApp.controller('ChromeOptionsMainController', function ChromeOption
   $scope.config  = config;
   $scope.page_id = $scope.config[0].id;
 
+  $scope.selectPage = function (id) {
+    $scope.page_id = id;
+  };
+
   options.ready.then(function () {
     $scope.options = options.categories;
   });
